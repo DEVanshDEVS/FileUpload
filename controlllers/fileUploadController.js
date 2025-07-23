e91ffcfe82af479a81436256746fa4321f0e3fb8
DEVanshDEVS <devanshdobhal983@gmail.com>
2025-07-23 12:43:41 +0530

    fileUploadController: Added imageSizeReducer route handler

--- controlllers/fileUploadController.js
+++ controlllers/fileUploadController.js
@@ -53,9 +53,15 @@ function isFileTypeSupported(type, supportedTypes){
 }
 
 //Function to upload to cloudinary
-async function uploadFileToCloudinary(file, folder) {
+async function uploadFileToCloudinary(file, folder, quality) {
   const options = {folder};
   console.log("temp file path: ", file.tempFilePath);
+  
+  //for reduce image size function
+  if (quality){
+    options.transformation = [{ quality }];
+  }
+
   options.resource_type = "auto";//detect file type automatically
   return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
@@ -161,3 +167,58 @@ exports.videoUpload = async (req,res) =>{
     });
   }
 };
+
+
+//image size reducer function
+exports.imageSizeReducer = async (req,res) => {
+  try{
+    //data fetch
+    const {name, tags, email} = req.body
+    console.log(name, tags, email);
+
+    const file = req.files.imageFile;
+    console.log(file);
+
+    //validation
+    const supportedTypes = ["jpeg", "jpg", "png"]; 
+    const fileType = file.name.split('.')[1].toLowerCase();
+    if(!isFileTypeSupported(fileType, supportedTypes)){
+      return res.status(400).json({
+        success:false,
+        message:'File format not supported',
+      });
+    }
+    //TODO: add an upper limit of 5MB
+    const maxSize = 5 * 1024 * 1024;
+    if (file.size > maxSize) {
+      return res.status(400).json({
+        success: false,
+        message: "File size exceeds 5MB limit",
+      });
+    }
+
+    //file format supported, upload to CLOUDINARY
+    const response = await uploadFileToCloudinary(file, "Folder1", 30);
+    console.log(response);
+
+    //db entry
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl:response.secure_url,
+    });
+
+    res.json({
+      success:true,
+      imageUrl:response.secure_url,
+      message:'Image Successfully Uploaded',
+    });
+  } catch(error){
+    console.error(error);
+    res.status(400).json({
+      success:false,
+      message:'Something went wrong',
+    });
+  }
+};  
--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {localFileUpload, imageUpload, videoUpload} = require("../controlllers/fileUploadController");
+const {localFileUpload, imageUpload, videoUpload, imageSizeReducer} = require("../controlllers/fileUploadController");
 
 router.post("/localFileUpload", localFileUpload);
 router.post("/imageUpload", imageUpload);
 router.post("/videoUpload", videoUpload);
+router.post("/imageSizeReducer", imageSizeReducer);
 
 module.exports = router;
